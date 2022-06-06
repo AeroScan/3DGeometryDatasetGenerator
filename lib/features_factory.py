8f560095087e3f30b897d1d3ca03e808e3e6faeb
PedroCorcaque <pedrollcorc@gmail.com>
2022-06-06 20:39:30 -0300

    Revert "BSplines Added"
    This reverts commit 26cf2487ba4b7d8e580b141ba65c9d30ff49c277.
    
    Revert to safe local on remote branch

--- lib/features_factory.py
+++ lib/features_factory.py
@@ -7,15 +7,11 @@ class FeaturesFactory:
         'cone': Cone,
         'sphere': Sphere,
         'torus': Torus,
-        'revolution': Revolution,
-        'extrusion': Extrusion,
-        'bsplinesurface': BSplineSurface,
     }
     CURVES_TYPES = {
         'line': Line,
         'circle': Circle,
         'ellipse': Ellipse,
-        'bsplinecurve': BSplineCurve,
     }
     
     @staticmethod
