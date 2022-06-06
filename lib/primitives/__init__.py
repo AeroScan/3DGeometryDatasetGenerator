0aff3eafe8c4e2311487fe8324039fe72a47788b
PedroCorcaque <pedrollcorc@gmail.com>
2022-06-06 20:59:41 -0300

    Removed some comments

--- lib/primitives/__init__.py
+++ lib/primitives/__init__.py
@@ -1,15 +1,3 @@
-# from .curves.line import Line
-# from .curves.bspline import BSplineCurve
-# from .curves.circle import Circle
-# from .curves.ellipse import Ellipse
-# from .surfaces.cone import Cone
-# from .surfaces.cylinder import Cylinder
-# from .surfaces.plane import Plane
-# from .surfaces.sphere import Sphere
-# from .surfaces.torus import Torus
-# from .surfaces.revolution import Revolution
-# from .surfaces.extrusion import Extrusion
-# from .surfaces.bspline import BSplineSurface
 from .curves import CurveFactory
 from .surfaces import SurfaceFactory
 from .base_curve_feature import BaseCurveFeature
