cc92a65e8478423b249d5e9820796ecae58fed3d
PedroCorcaque <pedrollcorc@gmail.com>
2024-01-07 23:28:28 -0300

    Fix the bug with non-completed faces

--- lib/generate_pythonocc.py
+++ lib/generate_pythonocc.py
@@ -1,8 +1,15 @@
+from tkinter.tix import MAX
 from tqdm import tqdm
 import numpy as np
 
-from OCC.Extend.DataExchange import read_step_file
+from OCC.Extend.DataExchange import read_step_file, read_step_file_with_names_colors
 from OCC.Extend.TopologyUtils import TopologyExplorer, list_of_shapes_to_compound
+from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Common
+from OCC.Core.BRep import BRep_Builder
+from OCC.Core.TopoDS import TopoDS_Compound
+from OCC.Core.BRep import BRep_Tool
+from OCC.Core.TopAbs import TopAbs_FACE
+from OCC.Core.TopoDS import TopoDS_Face
 
 from lib.generate_mesh_occ import OCCMeshGeneration, computeMeshData, searchEntityInMap
 from lib.tools import heal_shape, load_file_with_semantic_data, writeJSON, writeYAML
@@ -138,7 +145,7 @@ def processNoHighestDim(topology, generate_mesh):
     edges = [e for e in topology.edges()]
     faces = [f for f in topology.faces()]
     print(f"FACES: {len(faces)}")
-
+    # exit()
     geometries_data, mesh, faces_mesh_data, faces_map = processEdgesAndFaces(vertices, edges, faces, topology, generate_mesh)
 
     return geometries_data, mesh, faces_mesh_data, faces_map
@@ -165,32 +172,85 @@ def process(shape, generate_mesh=True, use_highest_dim=True):
     
     return geometries_data, mesh, faces_mesh_data, faces_map
 
+
+def find_corresponding_face(original_face, target_shape):
+    # Use the geometric properties to find the corresponding face in the target shape
+    original_surface = BRep_Tool.Surface(original_face)
+    
+    explorer = TopExp_Explorer(target_shape, TopAbs_FACE)
+    while explorer.More():
+        target_face = topods_Face(explorer.Current())
+        target_surface = BRep_Tool.Surface(target_face)
+        
+        # Compare the geometric properties, adjust the comparison criteria as needed
+        if original_surface.IsEqual(target_surface, 1e-6):  # Adjust tolerance as needed
+            return target_face
+        
+        explorer.Next()
+    
+    return None  # No corresponding face found
+
 def processPythonOCC(input_name: str, generate_mesh=True, use_highest_dim=True, scale_to_mm=1, debug=False, generate_semantic: bool = True, labels: list = ["tank", "pipe", "silo", "instrumentation", "floor", "wall", "structure"], semantic_name: str = './semantic'):
     shape = None
+    shape_filtered = None
     semantic_data = []
+    
     if generate_semantic: # To change by a new parameter
         if not labels:
             print("No labels found. Using default names from the file.")
-        shapes_list = load_file_with_semantic_data(input_name)
+
+        """ Read step file with names """
+        output = {}
+        output = read_step_file_with_names_colors(input_name)
         
-        shapes = []
-        for _shp, _name, _color in shapes_list:
-            # healed_shape = heal_shape(_shp, scale_to_mm)
-            shapes.append(_shp)
+        list_of_shapes = []
+        for shp, name_color_list in output.items():
+            shape = shp
+            name = name_color_list[0]
+            color = name_color_list[1]
+
+            list_of_shapes.append(shape)
 
             if labels:
-                if _name.lower() in labels:
-                    semantic_data.append((_name, _shp))
+                if name.lower() in labels:
+                    semantic_data.append((name, shape))
             else:
-                if _name:
-                    semantic_data.append((_name, _shp))
+                if name:
+                    semantic_data.append((name, shape))
+        shape_with_name, result = list_of_shapes_to_compound(list_of_shapes)
+        shape = shape_with_name
+        # shape_without_name      = read_step_file(input_name, as_compound=True)
 
-        shape, result = list_of_shapes_to_compound(shapes)
+        # topology_with_name    = TopologyExplorer(shape_with_name)
+        # topology_without_name = TopologyExplorer(shape_without_name)
+
+        # number_of_faces_with_names    = topology_with_name.number_of_faces()
+        # number_of_faces_without_names = topology_without_name.number_of_faces()
+
+        # print(f"number of faces with names: {number_of_faces_with_names}")
+        # print(f"number of faces without names: {number_of_faces_without_names}")
+
+        # print("Generating compound with BRepAlgoAPI_Common...")
+        # shape_filtered = BRepAlgoAPI_Common(shape_with_name, shape_without_name).Shape()
+        # topology_filtered = TopologyExplorer(shape_filtered)
+        
+        # number_of_faces_filtered = topology_filtered.number_of_faces()
+        # print(f"number of faces filtered: {number_of_faces_filtered}")
+        
+        # for face_with_name in topology_with_name.faces():
+        #     is_face_in_filtered = any(face_with_name.IsSame(face_filtered) for face_filtered in topology_filtered.faces())
+        #     if is_face_in_filtered:
+        #         print("There one")
+
+        # exit()
+
         if not result:
             print("Warning: all shapes were not added to the compound") # TODO: check this warning
     else:
         shape = read_step_file(input_name, verbosity=debug)
-        # shape = heal_shape(shape, scale_to_mm)
+        shape = heal_shape(shape, scale_to_mm)
+        # top_healed = TopologyExplorer(shape)
+        # faces_healed = [f for f in tqdm(top_healed.faces())] # 744 faces
 
     geometries_data, mesh, faces_mesh_data, faces_map_mesh = process(shape, generate_mesh=generate_mesh, use_highest_dim=use_highest_dim)
 
@@ -200,6 +260,7 @@ def processPythonOCC(input_name: str, generate_mesh=True, use_highest_dim=True, 
         semantic_data_dict["semantic"] = []
         faces_with_labels = []
         face_counter = 0
+
         for produto in semantic_data:
             """ produto list:
             ('Tank', <class 'TopoDS_Compound'>)
@@ -218,7 +279,9 @@ def processPythonOCC(input_name: str, generate_mesh=True, use_highest_dim=True, 
             vert_parameters = []
             face_indices = []
             for face in faces:
+
                 face_index = searchEntityInMap(face, faces_map_mesh)
+
                 if face_index == -1:
                     print("Warning: Face do not found in mesh!") # TODO: Check this
                     continue
@@ -237,40 +300,40 @@ def processPythonOCC(input_name: str, generate_mesh=True, use_highest_dim=True, 
                 }
             )
 
-        print(f"COUNTER_FACES: {face_counter}")
-        face_counter = 0
-        topology = TopologyExplorer(shape)
-        faces = [f for f in topology.faces()] # 1531
-        print(f">> FACES_OUT: {len(faces)}")
+        # print(f"COUNTER_FACES: {face_counter}")
+        # face_counter = 0
+        # topology = TopologyExplorer(shape)
+        # faces = [f for f in topology.faces()] # 1531
+        # print(f">> FACES_OUT: {len(faces)}")
 
-        count = 0
-        vert_indices = []
-        vert_parameters = []
-        face_indices = []
-        for face in faces:
-            face_index = searchEntityInMap(face, faces_map_mesh)
+        # count = 0
+        # vert_indices = []
+        # vert_parameters = []
+        # face_indices = []
+        # for face in faces:
+        #     face_index = searchEntityInMap(face, faces_map_mesh)
 
-            if face_index == -1:
-                print("WARNING HERE")
+        #     if face_index == -1:
+        #         print("WARNING HERE")
 
-            if face_index not in faces_with_labels:
-                count += 1
-                face_counter += len(faces_mesh_data[face_index]["face_indices"])
+        #     if face_index not in faces_with_labels:
+        #         count += 1
+        #         face_counter += len(faces_mesh_data[face_index]["face_indices"])
 
-                vert_indices += faces_mesh_data[face_index]["vert_indices"]
-                vert_parameters += faces_mesh_data[face_index]["vert_parameters"]
-                face_indices += faces_mesh_data[face_index]["face_indices"]
+        #         vert_indices += faces_mesh_data[face_index]["vert_indices"]
+        #         vert_parameters += faces_mesh_data[face_index]["vert_parameters"]
+        #         face_indices += faces_mesh_data[face_index]["face_indices"]
     
-        semantic_data_dict["semantic"].append(
-            {
-                "label": "unlabeled",
-                "vert_indices": np.unique(np.asarray(vert_indices)).tolist(),
-                "vert_parameters": np.unique(np.asarray(vert_parameters)).tolist(),
-                "face_indices": np.unique(np.asarray(face_indices)).tolist()
-            }
-        )
-        print(f">> semantic list: {len(semantic_data_dict['semantic'])}")
-        print(f"COUNTER_FACES: {face_counter}")
+        # semantic_data_dict["semantic"].append(
+        #     {
+        #         "label": "unlabeled",
+        #         "vert_indices": np.unique(np.asarray(vert_indices)).tolist(),
+        #         "vert_parameters": np.unique(np.asarray(vert_parameters)).tolist(),
+        #         "face_indices": np.unique(np.asarray(face_indices)).tolist()
+        #     }
+        # )
+        # print(f">> semantic list: {len(semantic_data_dict['semantic'])}")
+        # print(f"COUNTER_FACES: {face_counter}")
 
         writeJSON(semantic_name, semantic_data_dict)
         print("Done.\n")
